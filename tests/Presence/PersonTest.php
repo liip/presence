7a96e97824c6d59f81836f263da608c3e102e9a2
Bastian Feder <github@bastian-feder.de>
2013-09-28 22:03:22 +0200

    Tested: added first tests to verify the Person

--- tests/Presence/PersonTest.php
+++ tests/Presence/PersonTest.php
@@ -2,8 +2,37 @@
 
 namespace Presence;
 
-use \DateTime;
-
 class PersonTest extends PresenceTestCase
 {
+    public function testSetEvents()
+    {
+        $person = new Person('Tux');
+        $person->setEvents(array($this->getEventConfig()));
+
+        $this->assertAttributeContainsOnly('\Presence\Event', 'events',$person);
+    }
+
+    public function testSetEventsWithEmptyEventListProvided()
+    {
+        $person = new Person('Tux');
+
+        $this->setExpectedException('\Assert\InvalidArgumentException');
+        $person->setEvents(array());
+    }
+
+    public function testGetSchedule()
+    {
+        $calendar = $this->getMockBuilder('Presence\CalendarInterface')
+            ->setMethods(array('getSchedule'))
+            ->getMockForAbstractClass();
+        $calendar
+            ->expects($this->once())
+            ->method('getSchedule')
+            ->will($this->returnValue(array()));
+
+        $person = new Person('Tux');
+        $person->getSchedule($calendar);
+
+        $this->assertAttributeEmpty('events', $person);
+    }
 }
